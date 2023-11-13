c2259483bc52306dee76b86faa64e22329b3216b
Allan Gabriel <150678183+meAllanGarcia@users.noreply.github.com>
2023-11-13 09:47:46 -0300

    O site ainda está em desenvolvimento.
    O site está bem no começo de sua criação, então demorará um pouco para que eu consiga terminar e fazer um bom site para o trabalho em grupo.

---
+++ Trabalho - Manoel/script.js
@@ -0,0 +1,7 @@
+console.log ('Site criado para o trabalho no qual nos foi passado, pelo nosso professor Manoel, de RST (Relações Sociais Tecnológicas), da E.E. Rachid Jabur.')
+
+alert ('Demorei um cadin para fazer o site, mas é o que deu para fazer com meu breve e raso conhecimento.')
+alert ('O site foi feito por mim, Allan Garcia. Contudo, tive a ajuda de outros membros do grupo para com o desenvolvimento da pesquisa.')
+
+alert ('Dá 10, fessor!')
+
