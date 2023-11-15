035e878c711b82b65757fc85edc28cd75c43bfeb
Allan Gabriel <150678183+meAllanGarcia@users.noreply.github.com>
2023-11-15 18:56:31 -0300

    Add files via upload
    Signed-off-by: Allan Gabriel <150678183+meAllanGarcia@users.noreply.github.com>

--- Trabalho - Manoel/script.js
+++ Trabalho - Manoel/script.js
@@ -1,7 +1,21 @@
 console.log ('Site criado para o trabalho no qual nos foi passado, pelo nosso professor Manoel, de RST (Relações Sociais Tecnológicas), da E.E. Rachid Jabur.')
 
-alert ('Demorei um cadin para fazer o site, mas é o que deu para fazer com meu breve e raso conhecimento.')
-alert ('O site foi feito por mim, Allan Garcia. Contudo, tive a ajuda de outros membros do grupo para com o desenvolvimento da pesquisa.')
+// alert ('Demorei um cadin para fazer o site, mas é o que deu para fazer com meu breve e raso conhecimento.')
+// alert ('O site foi feito por mim, Allan Garcia. Contudo, tive a ajuda de outros membros do grupo para com o desenvolvimento da pesquisa.')
 
-alert ('Dá 10, fessor!')
+// alert ('Dá 10, fessor!')
 
+function temaB() {
+    document.body.style.background = '#f5f5f5'
+    document.body.style.color = '#000000'
+}
+
+function temaP() {
+    document.body.style.background = '#000000'
+    document.body.style.color = '#ffffff'
+}
+
+function temaC() {
+    document.body.style.background = '#424141'
+    document.body.style.color = '#ffffff'
+}
